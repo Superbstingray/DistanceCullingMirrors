f43f607a3b42441d910c5a1aa345ff658b5e421b
Superbstingray <74171114+Superbstingray@users.noreply.github.com>
2022-03-23 19:37:11 +1300

    Update SetMirrorCullDistances.cs

--- DistanceCullingMirrors/Scripts/SetMirrorCullDistances.cs
+++ DistanceCullingMirrors/Scripts/SetMirrorCullDistances.cs
@@ -23,15 +23,15 @@ namespace superbstingray
 
 	private GameObject MirrorCamObject;
 	private Camera MirrorCamera;
+	private bool isUpdated;
 
 		void OnValidate()
 		{
-			// Force Initialize Float Array in Editor if it is Null
+			// Force Initialize & Set Float Array to 32 Length
 			if  (LayerCullDistances == null) { LayerCullDistances = new float[32]; }
 		}
 		public void OnEnable()
 		{
-			// Force Float Array Size
 			if  (LayerCullDistances.Length != 32) { LayerCullDistances = new float[32]; }	
 			SendCustomEvent("_MirrorUpdate");
 		}
@@ -41,7 +41,9 @@ namespace superbstingray
 			if  (MirrorCamObject == null) { MirrorCamObject = GameObject.Find(string.Format("{0}{1}", "MirrorCam", gameObject.name)); }
 			if (Utilities.IsValid(MirrorCamObject))
 			{
-				// Update Specified Layers 
+				MirrorCamera = MirrorCamObject.GetComponent<Camera>();
+
+				// Update Player Layers Culling Distances 
 				if ((LayerCullDistances[9] == 0F)) // Player
 				{ LayerCullDistances[9] = PlayerCullingDistance;
 				}
@@ -51,6 +53,7 @@ namespace superbstingray
 				if ((LayerCullDistances[18] == 0F)) // MirrorReflection
 				{ LayerCullDistances[18] = PlayerCullingDistance;
 				}
+				// Update Pickup Related Layer Culling Distances
 				if ((LayerCullDistances[8] == 0F)) // Interactive
 				{ LayerCullDistances[8] = PickupCullingDistance;
 				}
@@ -60,27 +63,33 @@ namespace superbstingray
 				if ((LayerCullDistances[14] == 0F)) // PickupNoEnvironment
 				{ LayerCullDistances[14] = PickupCullingDistance;
 				}
-				// Update Unspecified Layers Distances to OtherCullingDistances
+
+				// Set Undefined Layers Distances to OtherCullingDistances
 				for(int i=0; i<32; i++)
 					if ((LayerCullDistances[i] == 0F)) { LayerCullDistances[i] = OtherCullingDistances; }
-
-				// Update Camera Name & Culling Properties
-				MirrorCamObject.name = string.Format("{0}{1}", gameObject.name, "_DCM");
-				MirrorCamera = MirrorCamObject.GetComponent<Camera>();
+			
+				// Update Camera Culling Properties & Values
 				MirrorCamera.layerCullDistances = LayerCullDistances;
 				MirrorCamera.layerCullSpherical = true;
 
-				// Log Mirror Update
-				Debug.Log(string.Format("[<color=yellow>DCM</color>] [<color=orange>{0}</color>] {1}", gameObject.name, "Applying Mirror Culling Distances"));
-				Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Player Culling Distance", PlayerCullingDistance));
-				Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Pickups Culling Distance", PickupCullingDistance));
-				Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Other Culling Distances", OtherCullingDistances));
+				if (!isUpdated)
+				{
+					// Update MirrorCam Name to Prevent Naming Conflicts
+					MirrorCamObject.name = string.Format("{0}{1}", gameObject.name, "_DCM");
 
+					// Log Mirror Update
+					Debug.Log(string.Format("[<color=yellow>DCM</color>] [<color=orange>{0}</color>] {1}", gameObject.name, "Applying Mirror Culling Distances"));
+					Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Player Culling Distance", PlayerCullingDistance));
+					Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Pickups Culling Distance", PickupCullingDistance));
+					Debug.Log(string.Format("[<color=yellow>DCM</color>] {0} <color=lightblue>{1}m</color>", "Other Culling Distances", OtherCullingDistances));
+				}
 				//Disable this Behaviour Once Completed
 				gameObject.GetComponent<SetMirrorCullDistances>().enabled = false;
+				isUpdated = true;
 
 			} else
-			{	// Retry if Mirror Camera Wasn't Initialized
+
+			{	// Retry if Mirror Camera Wasn't Initialized 
 				SendCustomEventDelayedSeconds("_MirrorUpdate", 1F);
 			}
 		}
